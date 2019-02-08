f9eaf8683e0563711cbf0cc092b8eb5b3e4045b6
Ferlix Yanto Wang <ferlixyantowang@yahoo.com>
2019-02-09 00:28:31 +0700

    Edit some stuffs for test flight requirement

--- cubeRun/cubeRun/GameViewController.swift
+++ cubeRun/cubeRun/GameViewController.swift
@@ -15,12 +15,6 @@ class GameViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setNeedsUpdateOfScreenEdgesDeferringSystemGestures()
-        
-//        let vignetteAtlas = SKTextureAtlas(named: "Vignette")
-//        let textureAtlases: [SKTextureAtlas] = [vignetteAtlas]
-//        SKTextureAtlas.preloadTextureAtlases(textureAtlases) {
-//            self.startScene()
-//        }
 
         self.startScene()
     }
@@ -45,7 +39,7 @@ class GameViewController: UIViewController {
     }
 
     override var shouldAutorotate: Bool {
-        return true
+        return false
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
